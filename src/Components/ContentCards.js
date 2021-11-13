ed1e800628af40fd399c2d65bd3ef25eea9dd00a
Mehmet Berkay Coruk <mehmetberkaycoruk@gmail.com>
2021-11-13 19:44:54 +0300

    database done(nearly), solve card rows

--- src/Components/ContentCards.js
+++ src/Components/ContentCards.js
@@ -1,6 +1,44 @@
 import React from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
+//const data = require("../data/gamedbfiltered.json");
+const gamedata = require("../data/gamedata.json");
+let gameinfo = [];
+let counter = 0;
+
+let traverse = () => {
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < gamedata[i].results.length; j++) {
+      console.log(gamedata[i].results[j]);
+      gameinfo[counter] = gamedata[i].results[j];
+      counter++;
+    }
+  }
+};
+
+let renderCard = (gameinfo, index) => {
+  return (
+    <>
+      <Col className="col-3">
+        <Card className="border-3" key={index}>
+          <Card.Img className="w-100" src={gameinfo.background_image} />
+          <Card.Body class="card-body">
+            <h5 className="card-title">{gameinfo.name}</h5>
+            <p className="card-text">
+              Rating: {gameinfo.rating} <br />
+              Genres: <br />
+              Ratings count: {gameinfo.ratings_count}
+            </p>
+            <Button className="btn btn-primary">
+              Add Href to these buttons or delete
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    </>
+  );
+};
+
 function ContentCards() {
   return (
     <div>
@@ -8,65 +46,10 @@ function ContentCards() {
         This is a special message: Make sure you either duplicate the cards if
         figured how to use the json files or restart the design
       </p>
+      <div>{traverse()}</div>
+
       <Container className="mt-2">
-        <Row>
-          <Col className="col-3">
-            <Card className="border-3">
-              <img
-                className="w-100"
-                src="https://media.rawg.io/media/games/da1/da1b267764d77221f07a4386b6548e5a.jpg"
-              ></img>
-              <Card.Body class="card-body">
-                <h5 class="card-title">Game Titles</h5>
-                <p class="card-text">
-                  Game Info maybe? make img tag after h5 to display game image
-                </p>
-                <Button class="btn btn-primary">
-                  Add Href to these buttons or delete
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="col-3">
-            <Card className="border-3">
-              <Card.Body class="card-body">
-                <h5 class="card-title">Game Titles</h5>
-                <p class="card-text">
-                  Game Info maybe? make img tag after h5 to display game image
-                </p>
-                <Button class="btn btn-primary">
-                  Add Href to these buttons or delete
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="col-3">
-            <Card className="border-3">
-              <Card.Body class="card-body">
-                <h5 class="card-title">Game Titles</h5>
-                <p class="card-text">
-                  Game Info maybe? make img tag after h5 to display game image
-                </p>
-                <Button class="btn btn-primary">
-                  Add Href to these buttons or delete
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="col-3">
-            <Card className="border-3">
-              <Card.Body class="card-body">
-                <h5 class="card-title">Game Titles</h5>
-                <p class="card-text">
-                  Game Info maybe? make img tag after h5 to display game image
-                </p>
-                <Button class="btn btn-primary">
-                  Add Href to these buttons or delete
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        <div>{gameinfo.map(renderCard)}</div>
       </Container>
     </div>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,83 +1,23 @@
 import React from "react";
 import { Row, Col, Container, Button } from "react-bootstrap";
-import { Link, Switch } from "react-router-dom";
-import GamePage from "../Pages/GamePage";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand btn btn-secondary" href="./GamePage">
-            GamePage
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="">
-                  GamePage
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr class="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link disabled"
-                  href="#"
-                  tabindex="-1"
-                  aria-disabled="true"
-                >
-                  Disabled
-                </a>
-              </li>
-            </ul>
-          </div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div className="container-fluid">
+          <Link to="/">
+            <Button className="btn btn-secondary p-1 m-auto">Home</Button>
+          </Link>
+
+          <ul className="navbar-nav ms-2 me-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/GamePage">
+                GamePage
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
